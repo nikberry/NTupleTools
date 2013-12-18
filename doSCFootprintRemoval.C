edc432e67f118a8b6ff019194beacff22422a693
Nik Berry <eepgnnb@pion2.brunel.ac.uk>
2013-12-18 12:44:09 +0000

    SCfootprint and script

---
+++ doSCFootprintRemoval.C
@@ -0,0 +1,6 @@
+git clone https://github.com/peruzzim/SCFootprintRemoval.git
+mkdir PFIsolation
+mv SCFootprintRemoval PFIsolation/
+cd PFIsolation
+mv SCFootprintRemoval SuperClusterFootprintRemoval 
+
